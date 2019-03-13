092cf62ef45548327b186da5f77b18f38d537b68
Omar Lozoya <23390638+omarlozoya@users.noreply.github.com>
2019-03-13 11:32:52 -0700

    Editing the gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
+var gulp = require("gulp");
+var browserSync = require("browser-sync").create();
+
+// gulp.task('hello', function() {
+//   console.log('Hello Omar');
+// });
 
 // Static server
 gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: './src',
-        },
-        open: false
-    });
+  browserSync.init({
+    server: {
+      baseDir: "./src"
+    },
+    open: false
+  });
 });
