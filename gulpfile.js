7bbe7701c44aab19b1c49094c7c8415e1b4c2a1d
Omar Lozoya <23390638+omarlozoya@users.noreply.github.com>
2019-03-13 19:48:40 -0700

    Messing with the package.json and serve in scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,72 +1,72 @@
-var gulp = require('gulp');
-var browsersync = require('browser-sync').create();
-// var sass = require('gulp-sass');
+// var gulp = require('gulp');
+// var browsersync = require('browser-sync').create();
+// // var sass = require('gulp-sass');
 
-// gulp.task('hello', function() {
-//   console.log('Hello Omar');
-// });
-// BrowserSync
-function browserSync(done) {
-  browsersync.init({
-    server: {
-      baseDir: 'src'
-    },
-    open: false
-  });
-  done();
-}
-
-// // BrowserSync Reload
-// function browserSyncReload(done) {
-//   browserSync.reload();
-//   done();
-// }
-
-// // CSS task
-// function css() {
-//   return gulp
-//     .src('src/scss/**/*.scss')
-//     .pipe(plumber())
-//     .pipe(sass({ outputStyle: "expanded" }))
-//     .pipe(gulp.dest('src/css'))
-//     .pipe(rename({ suffix: ".min" }))
-//     .pipe(postcss([autoprefixer(), cssnano()]))
-//     .pipe(gulp.dest('src/css'))
-//     .pipe(browsersync.stream());
-// }
-
-// // Watch files
-// function watchFiles() {
-//   gulp.watch("src/scss/**/*", css);
-// }
-
-// Static server
-// gulp.task('browserSync', function() {
-//   browserSync.init({
+// // gulp.task('hello', function() {
+// //   console.log('Hello Omar');
+// // });
+// // BrowserSync
+// function browserSync(done) {
+//   browsersync.init({
 //     server: {
 //       baseDir: 'src'
 //     },
 //     open: false
 //   });
 //   done();
-// });
+// }
 
-// gulp.task('sass', function() {
-//   return gulp
-//     .src('src/scss/**/*.scss')
-//     .pipe(sass())
-//     .pipe(gulp.dest('src/css'))
-//     .pipe(browserSync.reload({ stream: true }));
-// });
+// // // BrowserSync Reload
+// // function browserSyncReload(done) {
+// //   browserSync.reload();
+// //   done();
+// // }
 
-const watch = gulp.parallel(browserSync);
+// // // CSS task
+// // function css() {
+// //   return gulp
+// //     .src('src/scss/**/*.scss')
+// //     .pipe(plumber())
+// //     .pipe(sass({ outputStyle: "expanded" }))
+// //     .pipe(gulp.dest('src/css'))
+// //     .pipe(rename({ suffix: ".min" }))
+// //     .pipe(postcss([autoprefixer(), cssnano()]))
+// //     .pipe(gulp.dest('src/css'))
+// //     .pipe(browsersync.stream());
+// // }
 
-exports.watch = watch;
+// // // Watch files
+// // function watchFiles() {
+// //   gulp.watch("src/scss/**/*", css);
+// // }
 
-// // gulp.task('watch', ['browserSync', 'sass'], function() {
-// //   gulp.watch('src/scss/**/*.scss', ['sass']);
+// // Static server
+// // gulp.task('browserSync', function() {
+// //   browserSync.init({
+// //     server: {
+// //       baseDir: 'src'
+// //     },
+// //     open: false
+// //   });
+// //   done();
 // // });
 
-// // gulp.task('watch', ['browserSync', 'sass'], function() {
-// //   gulp.watch('src/scss/**/*.scss', ['sass']);
+// // gulp.task('sass', function() {
+// //   return gulp
+// //     .src('src/scss/**/*.scss')
+// //     .pipe(sass())
+// //     .pipe(gulp.dest('src/css'))
+// //     .pipe(browserSync.reload({ stream: true }));
 // // });
+
+// const watch = gulp.parallel(browserSync);
+
+// exports.watch = watch;
+
+// // // gulp.task('watch', ['browserSync', 'sass'], function() {
+// // //   gulp.watch('src/scss/**/*.scss', ['sass']);
+// // // });
+
+// // // gulp.task('watch', ['browserSync', 'sass'], function() {
+// // //   gulp.watch('src/scss/**/*.scss', ['sass']);
+// // // });
