e6a4a85dd655090f0112e572f75ba16febed24f9
Omar Lozoya <23390638+omarlozoya@users.noreply.github.com>
2019-03-13 21:32:54 -0700

    Trying to tell heroku to listen to a port via main.js file

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,19 @@
-const express = require('express');
-const app = express();
-app.listen(process.env.PORT);
-
 (function () {
   const win = window
   const doc = document.documentElement
+
+  var express = require('express')
+  var app = express()
+
+  app.set('port', (process.env.PORT || 5000))
+
+  //For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+  var result = 'App is running'
+  response.send(result);
+}).listen(app.get('port'), function() {
+  console.log('App is running, server is listening on port ', app.get('port'));
+});
 
   doc.classList.remove('no-js')
   doc.classList.add('js')
